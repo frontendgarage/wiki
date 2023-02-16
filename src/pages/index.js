4458f87f19b1328fcd16bba6eb8f0212c87d7e98
Kemil Beltre <kemilbeltre@gmail.com>
2023-02-16 22:51:09 +0100

    refactor: modularise code

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import useBaseUrl from "@docusaurus/useBaseUrl";
+import HomepageFeatures from "../components/HomepageFeatures";
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -28,53 +28,6 @@ function HomepageHeader() {
   );
 }
 
-function FeaturesSection() {
-    return (
-        <div className={clsx('margin-vert--lg', 'padding-vert--lg')}>
-            <div className="container">
-                <div className="row">
-                    <div className="col col--10 col--offset-1">
-                        <h2
-                            className={clsx(
-                                'text--center',
-                                'margin-bottom--xl',
-                            )}>
-                            Why Frontend Garage?
-                        </h2>
-                        <div className={clsx('row')}>
-                            <div className={clsx('col', 'col--4')}>
-                                <h3>💾 Data Structures</h3>
-                                <p>
-                                    Learn Data Structures form zero to hero with great examples.
-                                </p>
-                                <a href={useBaseUrl('data-structures')}>
-                                    <strong>Learn more</strong>
-                                </a>
-                            </div>
-                            <div className={clsx('col', 'col--4')}>
-                                <h3>🧠 Algorithms</h3>
-                                <p>
-                                    Algorithms is an essential topic that every Software Engineer should master.
-                                </p>
-                                <a href={useBaseUrl('algorithms')}>
-                                    <strong>Learn more</strong>
-                                </a>
-                            </div>
-                            <div className={clsx('col', 'col--4')}>
-                                <h3>💯 From zero to hero</h3>
-                                <p>
-                                    Go from zero to hero with Frontend Garage.
-                                    Learn the core of Software Development!
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -83,7 +36,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <FeaturesSection />
+        <HomepageFeatures />
       </main>
     </Layout>
   );
